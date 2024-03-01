24e917de553285cf124cfe1427093f816db5444b
HaxorSecInfec <102797531+HaxorSecInfec@users.noreply.github.com>
2024-03-01 07:37:26 +0700

    Update autoadduserwp.sh

--- autoadduserwp.sh
+++ autoadduserwp.sh
@@ -51,9 +51,10 @@ then
   echo -e "$cyan[+]$okegreen Successfull AddUser admin!"
   sleep 2
   echo " "
-  rm -rf wp.txt user.txt link.txt s.sql
+  rm -rf user.txt link.txt s.sql
 else
-  rm -rf wp.txt user.txt link.txt s.sql
   echo -e "$cyan[+]$red Failed AddUser admin!"
+    rm -rf user.txt link.txt s.sql
 fi
 done
+  rm -rf wp.txt user.txt link.txt s.sql
